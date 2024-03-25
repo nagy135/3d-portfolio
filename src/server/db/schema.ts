b0e72d71c2a524476294e5fc518eefafd73d392c
Viktor Nagy <viktor.nagy1995@gmail.com>
2024-03-25 14:35:51 +0100

    feat: init + model

---
+++ src/server/db/schema.ts
@@ -0,0 +1,37 @@
+// Example model schema from the Drizzle docs
+// https://orm.drizzle.team/docs/sql-schema-declaration
+
+import { sql } from "drizzle-orm";
+import {
+  index,
+  pgTableCreator,
+  serial,
+  timestamp,
+  varchar,
+} from "drizzle-orm/pg-core";
+
+/**
+ * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
+ * database instance for multiple projects.
+ *
+ * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
+ */
+export const createTable = pgTableCreator((name) => `3d-portfolio_${name}`);
+
+export const posts = createTable(
+  "model",
+  {
+    id: serial("id").primaryKey(),
+    name: varchar("name", { length: 256 }),
+    description: varchar("description", { length: 256 }),
+    modelPath: varchar("modelPath", { length: 256 }),
+    imagePath: varchar("imagePath", { length: 256 }),
+    createdAt: timestamp("created_at")
+      .default(sql`CURRENT_TIMESTAMP`)
+      .notNull(),
+    updatedAt: timestamp("updatedAt"),
+  },
+  (example) => ({
+    nameIndex: index("name_idx").on(example.name),
+  })
+);
